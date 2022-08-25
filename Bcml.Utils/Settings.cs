1ea4935070b6a1b788918a16fa05217dd1c014f3
ArchLeaders <archparliament@gmail.com>
2022-08-25 00:25:33 -0700

    Make comments useful; remove trimming

--- Bcml.Utils/Settings.cs
+++ Bcml.Utils/Settings.cs
@@ -2,41 +2,46 @@
 
 namespace Bcml.Utils
 {
-    /// <summary>
-    /// <para>Params as of 3.9.24</para>
-    /// <para></para>
-    /// <c>*str</c>: cemu_dir<br/>
-    /// <c>*str</c>: game_dir<br/>
-    /// <c>*str</c>: game_dir_nx<br/>
-    /// <c>*str</c>: update_dir<br/>
-    /// <c>*str</c>: dlc_dir<br/>
-    /// <c>*str</c>: dlc_dir_nx<br/>
-    /// <c>*str</c>: store_dir<br/>
-    /// <c>*str</c>: export_dir<br/>
-    /// <c>*str</c>: export_dir_nx<br/>
-    /// <c>bool</c>: load_reverse<br/>
-    /// <c>*str</c>: site_met""<br/>
-    /// <c>bool</c>: no_guess<br/>
-    /// <c>bool</c>: lang<br/>
-    /// <c>bool</c>: no_cemu<br/>
-    /// <c>bool</c>: wiiu<br/>
-    /// <c>bool</c>: no_hardlinks<br/>
-    /// <c>bool</c>: force_7z<br/>
-    /// <c>bool</c>: suppress_update<br/>
-    /// <c>bool</c>: loaded<br/>
-    /// <c>bool</c>: nsfw<br/>
-    /// <c>bool</c>: changelog<br/>
-    /// <c>bool</c>: strip_gfx<br/>
-    /// <c>bool</c>: auto_gb<br/>
-    /// <c>bool</c>: show_gb<br/>
-    /// <c>bool</c>: dark_theme<br/>
-    /// <c>*str</c>: last_version<br/>
-    /// </summary>
     public class Settings
     {
         public Dictionary<string, object?>? Data { get; }
 
+        /// <summary>
+        /// <para>Params as of 3.9.24</para>
+        /// <para></para>
+        /// <c>load_reverse</c><br/>
+        /// <c>no_guess</c><br/>
+        /// <c>lang</c><br/>
+        /// <c>no_cemu</c><br/>
+        /// <c>wiiu</c><br/>
+        /// <c>no_hardlinks</c><br/>
+        /// <c>force_7z</c><br/>
+        /// <c>suppress_update</c><br/>
+        /// <c>loaded</c><br/>
+        /// <c>nsfw</c><br/>
+        /// <c>changelog</c><br/>
+        /// <c>strip_gfx</c><br/>
+        /// <c>auto_gb</c><br/>
+        /// <c>show_gb</c><br/>
+        /// <c>dark_theme</c><br/>
+        /// </summary>
         public bool? GetBool(string name) => Data != null ? (Data![name] as bool?) : false;
+
+        /// <summary>
+        /// <para>Params as of 3.9.24</para>
+        /// <para></para>
+        /// <c>cemu_dir</c><br/>
+        /// <c>game_dir</c><br/>
+        /// <c>game_dir_nx</c><br/>
+        /// <c>update_dir</c><br/>
+        /// <c>dlc_dir</c><br/>
+        /// <c>dlc_dir_nx</c><br/>
+        /// <c>store_dir</c><br/>
+        /// <c>export_dir</c><br/>
+        /// <c>export_dir_nx</c><br/>
+        /// <c>site_meta</c><br/>
+        /// <c>last_version</c><br/>
+        /// </summary>
         public string? GetString(string name) => Data != null ? (Data![name] as string) : null;
 
         public Settings()
